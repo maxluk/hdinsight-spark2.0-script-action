43e97498f300aaa7ff73d5edd3edcc62e350a244
Eli Barzilay <eli@barzilay.org>
2016-08-22 18:54:59 -0400

    Improve script
    Use `curl` to get and pipe the tgz to `tar` in one shot; combine `rm`s;
    work in `/tmp` instead of `/root`.

--- install-spark2.0.sh
+++ install-spark2.0.sh
@@ -5,14 +5,11 @@ newspark="spark-2.0.0-bin-hadoop2.7"
 SPARK_DIR="$(readlink -f "/usr/hdp/current/spark-client")"
 SPARK_CONF_DIR="$(readlink -f "/usr/hdp/current/spark-client/conf")"
 
-cd "/root"
-wget "http://d3kbcqa49mib13.cloudfront.net/$newspark.tgz"
-tar -xzf "$newspark.tgz"
+cd "/tmp"
+curl "http://d3kbcqa49mib13.cloudfront.net/$newspark.tgz" | tar xzf -
 cd "$newspark"
-rm "jars/hadoop"*
-rm -r "conf"
+rm -r "jars/hadoop"* "conf"
 ln -s "$SPARK_CONF_DIR" "conf"
-
 cd ..
 rm -r "$SPARK_DIR"
 mv "$newspark" "$SPARK_DIR"

4c1c26d10522bcf4cbd26f1c1a56a88659ec2afb
Eli Barzilay <eli@barzilay.org>
2016-08-22 18:52:26 -0400

    Use a variable
    Instead of repeating the tgz file and the expected directory name.

--- install-spark2.0.sh
+++ install-spark2.0.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+newspark="spark-2.0.0-bin-hadoop2.7"
+
 SPARK_DIR="$(readlink -f "/usr/hdp/current/spark-client")"
 SPARK_CONF_DIR="$(readlink -f "/usr/hdp/current/spark-client/conf")"
 
 cd "/root"
-wget "http://d3kbcqa49mib13.cloudfront.net/spark-2.0.0-bin-hadoop2.7.tgz"
-tar -xzf "spark-2.0.0-bin-hadoop2.7.tgz"
-cd "spark-2.0.0-bin-hadoop2.7"
+wget "http://d3kbcqa49mib13.cloudfront.net/$newspark.tgz"
+tar -xzf "$newspark.tgz"
+cd "$newspark"
 rm "jars/hadoop"*
 rm -r "conf"
 ln -s "$SPARK_CONF_DIR" "conf"
 
 cd ..
 rm -r "$SPARK_DIR"
-mv "spark-2.0.0-bin-hadoop2.7" "$SPARK_DIR"
+mv "$newspark" "$SPARK_DIR"
 
 echo "Spark 2.0 installation completed"

a0c739bd2392495f035f2cb44cc3aa739b57f486
Eli Barzilay <eli@barzilay.org>
2016-08-22 18:42:39 -0400

    Remove unused stuff
    None of the `HDInsightUtilities-v01.sh` functions are used, so remove
    it.  Also no need for `$fullHostName`.  Cleanup some whitespaces, and
    the obscure comment.

--- install-spark2.0.sh
+++ install-spark2.0.sh
@@ -1,13 +1,4 @@
-#! /bin/bash
-
-# Link conf in root -> /etc/spark/2.4.1.1-3/0
-#
-
-# Import the helper method module.
-wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh && source /tmp/HDInsightUtilities-v01.sh && rm -f /tmp/HDInsightUtilities-v01.sh
-
-fullHostName=$(hostname -f)
-echo "fullHostName=$fullHostName"
+#!/bin/bash
 
 SPARK_DIR=$(readlink -f /usr/hdp/current/spark-client)
 SPARK_CONF_DIR=$(readlink -f /usr/hdp/current/spark-client/conf)
@@ -22,6 +13,6 @@ ln -s $SPARK_CONF_DIR conf
 
 cd ..
 rm -r $SPARK_DIR
-mv spark-2.0.0-bin-hadoop2.7 $SPARK_DIR 
+mv spark-2.0.0-bin-hadoop2.7 $SPARK_DIR
 
 echo "Spark 2.0 installation completed"
